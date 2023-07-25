1f4423339216b3f0d51a522f8797ba3c33bea995
Kagheni <Kaghenimbale@gmail.com>
2023-07-25 20:46:49 +0200

    Correct the react router dom

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Greeting from './Greeting';
-import { Route, Routes } from 'react-router';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
