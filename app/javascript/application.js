b8e0b8ada6063aa6bc0cc7685a9b7f255cb55598
Kagheni <Kaghenimbale@gmail.com>
2023-07-25 18:23:40 +0200

    Fetch my data to the ap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,7 +2,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-console.log(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+);
--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { fetchGreeting } from '../feature/greetingSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Greeting = () => {
-  return <div>Greeting</div>;
+  const { greeting, isLoading, error } = useSelector(
+    (store) => store.greetings,
+  );
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchGreeting());
+  }, [dispatch]);
+  return <h1>{greeting.greeting}</h1>;
 };
 
 export default Greeting;
